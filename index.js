9eba9c2179068406e4a1394f9878988a16dd8429
Gabriel de Farias de Macedo <87999378+Gabriel-d-Farias@users.noreply.github.com>
2021-12-15 18:54:39 -0300

    Bytebank projeto completo
    desenvolvido com Alura o projeto Bytebank utiliza da linguagem java pura

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 import {Cliente} from "./Cliente.js";
-import {Gerente} from "./Funcionario/Gerente.js";
-import {Diretor} from "./Funcionario/Diretor.js";
-import {SistemaAutenticacao} from "./SistemaAutenticacao.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const diretor = new Diretor("Rodrigo", 10000, 12345678900);
-diretor.cadastrarSenha("123456");
-const gerente = new Gerente("Ricardo", 5000, 12378945601);
+const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente =  new Gerente("Ricardo",  5000, 12378945601);
 gerente.cadastrarSenha("123");
 
-const cliente =  new Cliente("Lais", 78945612379, "456");
-
+const cliente = new Cliente("Lais", 78945612379, "456");
 const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
 const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+
 
 const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
 console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
 
-
-
-
-
-
-
-
-
-
-                                                /*  _      _      _
+                                                /*_      _      _
                                                 >(.)__ <(.)__ =(.)__
                                                 (___/  (___/  (___/ -Mark
                                                 */
